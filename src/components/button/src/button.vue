7702c7ec79310495950497e469197a63fdf4f680
yushare <582718643@qq.com>
2019-03-14 13:32:12 +0800

    build a component

---
+++ src/components/button/src/button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: 'keep-button'
+}
+</script>
---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Button from './button'
+
+const components = {
+  Button
+}
+
+const install = (Vue) => {
+  if (install.installed) return
+  // components.map(component => Vue.component(component.name, component))
+  Object.keys(components).forEach(key => {
+    Vue.component(components[key].name, components[key])
+  })
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+const API = {
+  install,
+  ...components
+}
+
+export default API
